127ead907cee7720c230bfd1c55b9979adc171fd
José Esteves <ajesteves@gmail.com>
2018-10-26 10:09:20 -0300

    Show Issues

--- main.ts
+++ main.ts
@@ -1,40 +1,45 @@
-var MAX_THREADS = 5;
+const MAX_THREADS = 5
 
-/**
- * Returns the array of cards that should be rendered for the current
- * e-mail thread. The name of this function is specified in the
- * manifest 'onTriggerFunction' field, indicating that this function
- * runs every time the add-on is started.
- *
- * @param {Object} e data provided by the Gmail UI.
- * @return {Card[]}
- */
-function buildAddOn(e) {
-  Logger.log("HOLA QUE TAL")
-  Logger.log(JSON.parse(accessProtectedResource("https://api.github.com/repos/ponyesteves/help-desk/issues"))[0].title)
-  // Activate temporary Gmail add-on scopes.
-  var accessToken = e.messageMetadata.accessToken;
-  GmailApp.setCurrentMessageAccessToken(accessToken);
-
-  var messageId = e.messageMetadata.messageId;
-  var senderData = extractSenderData(messageId);
-  var cards = [];
-
-  // Build a card for each recent thread from this email's sender.
-  if (senderData.recents.length > 0) {
-    senderData.recents.forEach(function(threadData) {
-      cards.push(buildRecentThreadCard(senderData.email, threadData));
-    });
-  } else {
-    // Present a blank card if there are no recent threads from
-    // this sender.
-    cards.push(CardService.newCardBuilder()
-      .setHeader(CardService.newCardHeader()
-        .setTitle('No recent threads from this sender')).build());
-  }
-  return cards;
+interface Issue {
+  title: string
+  body: string
+  state: string
+  url: string
 }
 
+function buildAddOn(e) {
+  const issues: Issue[] = JSON.parse(accessProtectedResource(
+    'https://api.github.com/repos/ponyesteves/help-desk/issues'
+  ))
+
+  // Activate temporary Gmail add-on scopes.
+  const accessToken = e.messageMetadata.accessToken
+  GmailApp.setCurrentMessageAccessToken(accessToken)
+
+  const messageId = e.messageMetadata.messageId,
+    senderData = extractSenderData(messageId)
+  let cards = []
+
+  if (issues.length > 0) {
+    issues.forEach(issue => {
+      cards = [...cards, buildIssueCard(issue)]
+    })
+  } else {
+    cards = [emptyCard()]
+  }
+
+  return cards
+}
+
+function emptyCard(){
+  CardService.newCardBuilder()
+  .setHeader(
+    CardService.newCardHeader().setTitle(
+      'No recent threads from this sender'
+    )
+  )
+  .build()
+}
 /**
  *  This function builds a set of data about this sender's presence in your
  *  inbox.
@@ -44,49 +49,56 @@ function buildAddOn(e) {
  */
 function extractSenderData(messageId) {
   // Use the Gmail service to access information about this message.
-  var mail = GmailApp.getMessageById(messageId);
-  var threadId = mail.getThread().getId();
-  var senderEmail = extractEmailAddress(mail.getFrom());
-
-  var recentThreads = GmailApp.search('from:' + senderEmail);
-  var recents = [];
-
-  // Retrieve information about up to 5 recent threads from the same sender.
-  recentThreads.slice(0, MAX_THREADS).forEach(function(thread) {
-    if (thread.getId() != threadId && ! thread.isInChats()) {
-      recents.push({
-        'subject': thread.getFirstMessageSubject(),
-        'count': thread.getMessageCount(),
-        'link': 'https://mail.google.com/mail/u/0/#inbox/' + thread.getId(),
-        'lastDate': thread.getLastMessageDate().toDateString()
-      });
-    }
-  });
-
-  var senderData = {
+  const mail = GmailApp.getMessageById(messageId),
+    senderEmail = extractEmailAddress(mail.getFrom())
+  return {
     email: senderEmail,
-    recents: recents
-  };
-
-  return senderData;
+  }
 }
 
-/**
- *  Given the result of GmailMessage.getFrom(), extract only the email address.
- *  getFrom() can return just the email address or a string in the form
- *  "Name <myemail@domain>".
- *
- *  @param {String} sender The results returned from getFrom().
- *  @return {String} Only the email address.
- */
 function extractEmailAddress(sender) {
-  var regex = /\<([^\@]+\@[^\>]+)\>/;
-  var email = sender; // Default to using the whole string.
-  var match = regex.exec(sender);
+  var regex = /\<([^\@]+\@[^\>]+)\>/
+  var email = sender // Default to using the whole string.
+  var match = regex.exec(sender)
   if (match) {
-    email = match[1];
+    email = match[1]
   }
-  return email;
+  return email
+}
+
+function buildIssueCard(issue: Issue) {
+  var card = CardService.newCardBuilder()
+  card.setHeader(CardService.newCardHeader().setTitle(issue.title))
+  var section = CardService.newCardSection().setHeader(
+    '<font color="#1257e0">Issue #___</font>'
+  )
+  section.addWidget(CardService.newTextParagraph().setText(issue.body))
+  // section.addWidget(
+  //   CardService.newKeyValue()
+  //     .setTopLabel('Sender')
+  //     .setContent(senderEmail)
+  // )
+  // section.addWidget(
+  //   CardService.newKeyValue()
+  //     .setTopLabel('Number of messages')
+  //     .setContent(threadData.count.toString())
+  // )
+  // section.addWidget(
+  //   CardService.newKeyValue()
+  //     .setTopLabel('Last updated')
+  //     .setContent(threadData.lastDate.toString())
+  // )
+
+  const threadLink = CardService.newOpenLink()
+    .setUrl(issue.url)
+    .setOpenAs(CardService.OpenAs.FULL_SIZE)
+  var button = CardService.newTextButton()
+    .setText('Open Thread')
+    .setOpenLink(threadLink)
+  section.addWidget(CardService.newButtonSet().addButton(button))
+
+  card.addSection(section)
+  return card.build()
 }
 
 /**
@@ -96,30 +108,38 @@ function extractEmailAddress(sender) {
  *  @param {Object} threadData Infomation about the thread to display.
  *  @return {Card} a card that displays thread information.
  */
+
 function buildRecentThreadCard(senderEmail, threadData) {
-  var card = CardService.newCardBuilder();
-  card.setHeader(CardService.newCardHeader().setTitle(threadData.subject));
-  var section = CardService.newCardSection()
-    .setHeader('<font color=\"#1257e0\">Recent thread</font>');
-  section.addWidget(CardService.newTextParagraph().setText(threadData.subject));
-  section.addWidget(CardService.newKeyValue()
-    .setTopLabel('Sender')
-    .setContent(senderEmail));
-  section.addWidget(CardService.newKeyValue()
-    .setTopLabel('Number of messages')
-    .setContent(threadData.count.toString()));
-  section.addWidget(CardService.newKeyValue()
-    .setTopLabel('Last updated')
-    .setContent(threadData.lastDate.toString()));
+  var card = CardService.newCardBuilder()
+  card.setHeader(CardService.newCardHeader().setTitle(threadData.subject))
+  var section = CardService.newCardSection().setHeader(
+    '<font color="#1257e0">Recent thread</font>'
+  )
+  section.addWidget(CardService.newTextParagraph().setText(threadData.subject))
+  section.addWidget(
+    CardService.newKeyValue()
+      .setTopLabel('Sender')
+      .setContent(senderEmail)
+  )
+  section.addWidget(
+    CardService.newKeyValue()
+      .setTopLabel('Number of messages')
+      .setContent(threadData.count.toString())
+  )
+  section.addWidget(
+    CardService.newKeyValue()
+      .setTopLabel('Last updated')
+      .setContent(threadData.lastDate.toString())
+  )
 
   var threadLink = CardService.newOpenLink()
     .setUrl(threadData.link)
-    .setOpenAs(CardService.OpenAs.FULL_SIZE);
+    .setOpenAs(CardService.OpenAs.FULL_SIZE)
   var button = CardService.newTextButton()
-    .setText('Open Thread')
-    .setOpenLink(threadLink);
-  section.addWidget(CardService.newButtonSet().addButton(button));
+    .setText('Open Issue')
+    .setOpenLink(threadLink)
+  section.addWidget(CardService.newButtonSet().addButton(button))
 
-  card.addSection(section);
-  return card.build();
+  card.addSection(section)
+  return card.build()
 }
